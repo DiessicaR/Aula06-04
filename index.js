5b83dfece5b223730f0acc5eef0cc3279f88dd91
Diessica <diessicarrc@gmail.com>
2023-04-06 21:50:39 -0300

    aula prompt alert

---
+++ index.js
@@ -0,0 +1,75 @@
+/* exercicio 1
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+console.log(a+b)
+
+//exercicio 2
+
+let x = 10
+let y = 20
+
+z = x
+y = z
+x = y 
+
+console.log (x + y + z)
+console.log (x, y, z)
+
+exercicio 3 
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+let resultadoSalario = t / p
+alert("Você recebe R$" + resultadoSalario + " por hora")
+*/
+
+
+// Declare uma variável para armazenar um nome, sem atribuir um valor
+
+let nome = null;
+
+// Declare uma variável para armazenar uma idade, sem atribuir um valor
+
+let idade = null
+/*typeof idade;
+typeof nome;
+
+alert(typeof idade);
+alert(typeof nome);
+
+console.log(typeof idade);
+console.log(typeof nome);
+
+//Pergunte ao usuário seu nome e sua idade, atribuindo esses dois valores às variáveis que acabou de criar.
+nome = prompt('Qual seu nome?')
+idade = prompt('Qual sua idade?')
+
+//Para finalizar, imprima na tela a mensagem: "Olá nome,  você tem idade anos". Onde nome e idade são os valores que o usuário inseriu
+alert("Olá " + nome + ", você tem " + idade + " anos")
+console.log("Olá " + nome + ", você tem " + idade + " anos")
+*/
+
+
+ quest1 = "Você gosta de chimarrão? "
+ quest2 = "Você gosta de assistir musical? "
+ quest3 = "Você já viajou para outro país? "
+
+
+
+ resposta1 = prompt(quest1)
+ resposta2 = prompt(quest2)
+ resposta3 = prompt(quest3)
+
+alert(quest1 + resposta1)
+alert(quest2 + resposta2)
+alert(quest3 + resposta3)
+
+
+console.log( quest1, resposta1)
+console.log(quest2, resposta2)
+console.log(quest3, resposta3)
